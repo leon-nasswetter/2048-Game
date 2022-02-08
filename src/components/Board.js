153fdac0ee76bbfcfee87eafaeb1cf9d80b29c75
Leon Nasswetter <leon.nasswetter@gmail.com>
2022-02-07 16:41:05 -0800

    setup

---
+++ src/components/Board.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Tile from "./Tile"
+import Cell from "./Cell"
+
+const Board = () => {
+    return (
+        <div>
+            Board
+        </div>
+    )
+}
+
+export default Board
--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import "./main.scss";
+import "./styles.scss";
+
 
 const App = () => {
   return <h1> 2048 </h1>
